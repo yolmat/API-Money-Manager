54feb7664c49b7cab49fc74a3dc9dfd785002424
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-07-05 04:29:17 -0300

    create validation yup users

---
+++ src/validation/user.validation.js
@@ -0,0 +1,7 @@
+import * as yup from "yup"
+
+export const userValidation = yup.object({
+    name: yup.string().required(),
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6)
+})
